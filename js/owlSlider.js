ced621bc6717d6189cf2f6e278a64f0b7b6f7eed
camrambo <49794860+camrambo@users.noreply.github.com>
2020-05-01 10:45:41 -0600

    Update owlSlider.js

--- js/owlSlider.js
+++ js/owlSlider.js
@@ -1,6 +1,6 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
-    margin: 10,
+    margin: 0,
     nav: true,
     dots: false,
     navText : ['<i class="fas fa-chevron-circle-left"></i>','<i class="fas fa-chevron-circle-right"></i>'],
