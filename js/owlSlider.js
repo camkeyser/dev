69099bbe0a55e5dd6e2d021b4bd99a85be8e0729
camrambo <49794860+camrambo@users.noreply.github.com>
2020-05-01 10:22:12 -0600

    Add files via upload

---
+++ js/owlSlider.js
@@ -0,0 +1,8 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    navText : ['<i class="fas fa-chevron-circle-left"></i>','<i class="fas fa-chevron-circle-right"></i>'],
+    items:1
+});
